9ba0bcfa31063a274e03b6122ba6f2f7348aeec4
Hamza Lhamous <hamza.lhamous@gmail.com>
2023-07-12 13:30:35 -0700

    outline of js process

---
+++ assets/Script/script.js
@@ -0,0 +1,18 @@
+//Variables
+
+
+//Event listener to startQuiz
+//Function to initialize startQuiz() and startQuizTimer()
+
+//Function to show multiple choice questions
+
+//Function to show user whether their answer is true or false --use boolean
+
+//Function to endQuiz() when timer ends and prompt user for their initials
+
+//Function for local storage to saveScores() based on initials and correct answers out of total questions
+//calculate the variable highScores
+
+//Function to show highScores
+//create goBack btn and clearHighScores btn
+//create event listener on the parent to apply to both btns-- or maybe one for each??
